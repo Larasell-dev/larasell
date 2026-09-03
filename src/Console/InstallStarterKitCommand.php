1da6282edb6808e7a3d59b1a6bcdbe69d3df9d77
Nils Haberkamp <haberkamp.n@gmail.com>
2026-09-03 18:52:50 +0200

    chore: get rid of node type package

--- src/Console/InstallStarterKitCommand.php
+++ src/Console/InstallStarterKitCommand.php
@@ -103,7 +103,6 @@ class InstallStarterKitCommand extends Command
                 '--save-dev',
                 '@types/react',
                 '@types/react-dom',
-                '@types/node',
                 '@vitejs/plugin-react',
                 'typescript',
             ]);
