25bd6de2207452c15acf4ad633bbdda8b12dc91a
Nils Haberkamp <haberkamp.n@gmail.com>
2026-09-09 21:04:12 +0200

    chore: add tailwind to starter kit

--- stubs/starter-kit/vite.config.js
+++ stubs/starter-kit/vite.config.js
@@ -1,14 +1,16 @@
 import inertia from '@inertiajs/vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/js/app.tsx'],
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
       refresh: true,
     }),
+    tailwindcss(),
     inertia(),
     react(),
   ],
