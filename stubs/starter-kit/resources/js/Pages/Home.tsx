650f71f775e7fde473f320ea5e5e70147186ff65
Nils Haberkamp <haberkamp.n@gmail.com>
2026-09-10 23:30:36 +0200

    perf: prefetch cart, product and category pages

--- stubs/starter-kit/resources/js/Pages/Home.tsx
+++ stubs/starter-kit/resources/js/Pages/Home.tsx
@@ -18,17 +18,17 @@ function Home() {
     <main className="grid flex-1 lg:grid-cols-2 lg:grid-rows-2">
       <Head title="Home" />
       {categories[0] && (
-        <Link className={`${linkClass} max-lg:border-b lg:row-span-2 lg:border-r`} href={categories[0].url}>
+        <Link className={`${linkClass} max-lg:border-b lg:row-span-2 lg:border-r`} href={categories[0].url} prefetch>
           {categories[0].name}
         </Link>
       )}
       {categories[1] && (
-        <Link className={`${linkClass} border-b`} href={categories[1].url}>
+        <Link className={`${linkClass} border-b`} href={categories[1].url} prefetch>
           {categories[1].name}
         </Link>
       )}
       {categories[2] && (
-        <Link className={linkClass} href={categories[2].url}>
+        <Link className={linkClass} href={categories[2].url} prefetch>
           {categories[2].name}
         </Link>
       )}
